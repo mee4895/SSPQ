9a772cbb01fa0b8d3d5e22bd7212a3c80bce4b05
Tobias Wältken <tobias.w@ltken.de>
2018-05-27 11:38:42 +0200

    Add retry setting to client cli

--- client.py
+++ client.py
@@ -69,20 +69,20 @@ class Client():
         self.connected = False
 
 
-async def _send_msg(message: str, host: str, port: int, loop):
+async def _send_msg(message: str, host: str, port: int, retrys: int, loop):
     client = Client()
     await client.connect(host=host, port=port, loop=loop)
-    await client.send(message.encode())
+    await client.send(message.encode(), retrys=retrys)
     client.disconnect()
 
 
-async def _receive_msg(host: str, port: int, loop):
+async def _receive_msg(host: str, port: int, nac: bool, loop):
     client = Client()
     await client.connect(host=host, port=port, loop=loop)
     msg = await client.receive()
     print('Message:')
     print(msg.decode())
-    if not NAC:
+    if not nac:
         await client.confirm()
         print('(Auto-confirmed message)')
     client.disconnect()
@@ -97,22 +97,19 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--address', action='store', default='127.0.0.1', required=False, help='Set the server address to connect to.', dest='host', metavar='<host>')
     parser.add_argument('-p', '--port', action='store', default=SSPQ_PORT, type=int, required=False, help='Set the port the server listens to', dest='port', metavar='<port>')
     parser.add_argument('-m', '--message', action='store', default='', required=False, help='Set the message to send', dest='message', metavar='<message>')
+    parser.add_argument('--retrys', action='store', default='3', type=int, required=False, help='Set the amount of retrys for failed messages', dest='retrys', metavar='<retrys>')
     parser.add_argument('-nac', '--no-auto-confirm', action='store_true', required=False, help='Disable auto confirm. WARNING this automatically requeues the message since the conection is terminated after the command finishes', dest='nac')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s v.0.2.0')
     args = parser.parse_args()
 
-    # react to args
+    # check args
     if args.send and args.message == '':
         parser.error('The usage of empty messages is discuraged. Please add a message!')
-    HOST = args.host
-    PORT = args.port
-    NAC = args.nac
-    message = args.message
 
     # setup asyncio
     loop = asyncio.get_event_loop()
     if args.send:
-        loop.run_until_complete(_send_msg(message, host=HOST, port=PORT, loop=loop))
+        loop.run_until_complete(_send_msg(args.message, host=args.host, port=args.port, retrys=args.retrys, loop=loop))
     if args.receive:
-        loop.run_until_complete(_receive_msg(host=HOST, port=PORT, loop=loop))
+        loop.run_until_complete(_receive_msg(host=args.host, port=args.port, nac=args.nac, loop=loop))
     loop.close()
